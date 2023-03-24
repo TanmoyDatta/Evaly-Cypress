3a61eee31c81dd60a6d37654e533087f47c92adb
Tanmoy Datta <tonmoydatta45@gmail.com>
2023-03-25 02:28:47 +0600

    second commit in cypress

--- cypress/e2e/Basic/Test.cy.js
+++ cypress/e2e/Basic/Test.cy.js
@@ -6,15 +6,6 @@ describe('OPEN URL', () => {
 
   let signUp_BTN = "//span[@class='hidden md:inline-block']";
 
-  //   it('Test', () => {
-  //     cy.xpath(signUp_BTN).click();
-  //     cy.get('.mt-12 > :nth-child(1) > .relative > .block').type('mrtanmoy64@gmail.com');
-  //     cy.wait(500);
-  //     cy.get('.mt-4 > label.block > .relative > .block').type('password123#');
-  //     cy.get('.btn').click();
-  //     cy.wait(2000);
-  //   });
-
   let search_enter =
     "//div[@class='hidden w-full md:flex px-4']//input[@id='searchbar']";
   let search_BTN =
@@ -22,11 +13,25 @@ describe('OPEN URL', () => {
   let shotBy = "//span[normalize-space()='Price']";
   let checkbon_Smartwatches = "//p[normalize-space()='Smartwatches']";
 
-  it('Home Page', () => {
+  it('Login Page Test', () => {
+    cy.xpath(signUp_BTN).click();
+    cy.get('.mt-12 > :nth-child(1) > .relative > .block').type(
+      'mrtanmoy64@gmail.com'
+    );
+    cy.wait(500);
+    cy.get('.mt-4 > label.block > .relative > .block').type('password123#');
+    cy.get('.btn').click();
+    cy.wait(2000);
+
     cy.xpath(search_enter).type('Smart Watch');
     cy.xpath(search_BTN).click();
     cy.xpath(shotBy).click();
-
-    //  cy.xpath(checkbon_Smartwatches).click();
   });
+
+  // it('Home Page Test', () => {
+  //   //  cy.xpath(search_enter).type('Smart Watch');
+  //   //  cy.xpath(search_BTN).click();
+  //   //  cy.xpath(shotBy).click();
+  //   //  cy.xpath(checkbon_Smartwatches).click();
+  // });
 });
